d83e1abb5b48c87f5bb4c950fb4ca0cdab1eb177
Noodl <havefun366@gmail.com>
2026-01-10 00:21:52 -0500

    Add files via upload

--- airride.js
+++ airride.js
@@ -252,7 +252,8 @@ function renderSrcTable(mountId, rows, ctx) {
   if (!mount) return;
 
   const COLS = ["Player","Time","Machine","Rider","SRC Link","Video"];
-  const colgroup = `
+  const isMobile = window.matchMedia('(max-width: 900px)').matches;
+  const colgroup = isMobile ? '' : `
     <colgroup>
       <col style="width:18%">
       <col style="width:16%">
